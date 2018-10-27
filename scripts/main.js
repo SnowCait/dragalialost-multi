d7d326251c0012c09ba4581557abac822f62b2ad
SnowCait <SnowCait@users.noreply.github.com>
2018-10-27 22:56:40 +0900

    Install Firebase

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD4bCEtnnHj_6_9KH4HwMuc5k-iWzeGlNo",
+    authDomain: "dragalialost-multi.firebaseapp.com",
+    databaseURL: "https://dragalialost-multi.firebaseio.com",
+    projectId: "dragalialost-multi",
+    storageBucket: "dragalialost-multi.appspot.com",
+    messagingSenderId: "894548322461"
+};
+firebase.initializeApp(config);
+
+console.log('Firebase is initialized.', firebase);
