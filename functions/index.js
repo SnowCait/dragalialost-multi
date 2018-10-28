71e4d71911d844ff73bdecc9168ee973eff59ff4
SnowCait <SnowCait@users.noreply.github.com>
2018-10-29 06:20:28 +0900

    Subscribe topics

--- functions/index.js
+++ functions/index.js
@@ -2,24 +2,59 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
+const serviceAccount = require('./serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 const messaging = admin.messaging();
 
 exports.createRoom = functions.https.onRequest((req, res) => {
-  const registrationToken = '';
+  const topic = 'test-topic-1';
   const payload = {
     notification: {
       title: 'TITLE',
       body: 'BODY'
     },
-    token: registrationToken
+    topic: topic
   };
+  
   messaging.send(payload)
   .then((response) => {
     console.log('Success to send message.', response);
   })
   .catch((error) => {
-    console.error('Failed to send message.', error)
+    console.error('Failed to send message.', error);
   });
-  return res.status(204).send();
+  
+  return res.status(200).send();
+});
+
+exports.subscribeQuest = functions.https.onRequest((req, res) => {
+  const registrationToken = '';
+  const topic = 'test-topic-1';
+  
+  messaging.subscribeToTopic(registrationToken, topic)
+    .then((response) => {
+      console.log('Successfully subscribed to topic.', response);
+    })
+    .catch((error) => {
+      console.error('Error subscribing to topic.', error);
+    });
+  
+  return res.status(200).send();
+});
+
+exports.unsubscribeQuest = functions.https.onRequest((req, res) => {
+  const registrationToken = '';
+  const topic = 'test-topic-1';
+  
+  messaging.unsubscribeFromTopic(registrationToken, topic)
+    .then((response) => {
+      console.log('Successfully unsubscribed from topic.', response);
+    })
+    .catch((error) => {
+      console.error('Error unsubscribing from topic.', error);
+    });
+  
+    return res.status(200).send();
 });
