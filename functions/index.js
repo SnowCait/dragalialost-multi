c963967f8cb6db6bfac1254a5d9018e1a4ddc6a5
SnowCait <SnowCait@users.noreply.github.com>
2018-11-04 04:13:56 +0900

    Retrieve subscribing quests

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,22 @@ admin.initializeApp({
 });
 const messaging = admin.messaging();
 
+const request = require('request');
+function requestAsync(options) {
+  return new Promise(function (resolve, reject) {
+    request(options, function (error, res, body) {
+      if (!error && res.statusCode == 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+// できれば admin から取得したい
+const serverKey = '';
+
 exports.createRoom = functions.https.onRequest((req, res) => {
   console.log('Requested.', req.url, req.body);
   const topic = req.body.topic;
@@ -42,6 +58,37 @@ exports.createRoom = functions.https.onRequest((req, res) => {
   });
   
   return res.status(200).end();
+});
+
+exports.getSubscribingQuests = functions.https.onRequest(async (req, res) => {
+  console.log('Requested.', req.url, req.body);
+  const userToken = req.body.token;
+
+  // Validation
+  if (!userToken) {
+    console.error('UserToken:', userToken);
+    return res.status(400).end();
+  }
+
+  const options = {
+    method: 'GET',
+    url: `https://iid.googleapis.com/iid/info/${userToken}?details=true`,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `key=${serverKey}`
+    }
+  };
+  try {
+    const json = await requestAsync(options);
+    const result = JSON.parse(json);
+    const topics = result.rel ? result.rel.topics : {};
+    console.log('Topics:', topics);
+
+    return res.status(200).send(JSON.stringify(topics));
+  } catch (err) {
+    console.error(err);
+    return res.status(500).end();
+  }
 });
 
 exports.subscribeQuest = functions.https.onRequest((req, res) => {
